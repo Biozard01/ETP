03ae288a2ed304dbc54f26d576306397d480c89d
Biozard01 <arthur.laforest33@gmail.com>
2021-04-20 10:49:01 +0200

    arrow display and logic v1

--- EffraieTonPote/MainWindow.xaml.cs
+++ EffraieTonPote/MainWindow.xaml.cs
@@ -25,101 +25,129 @@ namespace EffraieTonPote
             InitializeComponent();
 
         }
+        private void Play(object sender, RoutedEventArgs e)
+        {
+            TextBlock0.Text = "Direction : ";
+            TextBlock1.Text = "";
+            TextBlock1.Background = Brushes.LightBlue;
+            TextBlock2.Text = "Scores : 0";
+            TextBlock3.Text = "";
+            Arrow.Visibility = Visibility.Hidden;
+            RotateTransform rotateTransform = new RotateTransform(0);
+            Arrow.RenderTransform = rotateTransform;
+        }
+
+        private void Reset(object sender, RoutedEventArgs e)
+        {
+            TextBlock0.Text = "Direction : ";
+            TextBlock1.Background = Brushes.LightBlue;
+            TextBlock2.Text = "Scores : 0";
+            TextBlock3.Text = "";
+            Arrow.Visibility = Visibility.Hidden;
+            RotateTransform rotateTransform = new RotateTransform(0);
+            Arrow.RenderTransform = rotateTransform;
+        }
 
         private void KeyHandler(object sender, KeyEventArgs e)
         {
             List<int> arr = new List<int>();
+            int points = 0;
+            Random random = new Random();
+            string picture = "C:\\Users\\arthu\\source\\repos\\ETP\\EffraieTonPote\\arrow.png";
+            Arrow.Source = new BitmapImage(new Uri(picture));
 
-            /* list for the arrow direction init */
-            for (int i = 0; i < 5; i++)
+            for (int i = 0; i <= 4; i++)
             {
-                Random random = new Random();
-                int pick = random.Next(1, 5);
-                arr.Add(pick);
+                arr.Add(i);
             }
 
+            int pick = random.Next(1, arr.Count);
 
-            foreach (var i in arr)
+            TextBlock3.Text = "Gen Number " + pick;
+
+            /* arrow direction display */
+
+            Arrow.Visibility = Visibility.Visible;
+
+            if (pick == 1)
             {
-                int points = 0;
+                RotateTransform rotateTransform = new RotateTransform(-90);
+                Arrow.RenderTransform = rotateTransform;
 
-                TextBlock3.Text = "Gen Number " + arr[i];
+                string sarrowdir = "haut";
+                TextBlock0.Text = "Direction : " + sarrowdir;
+            }
 
-                /* arrow direction display */
+            else if (pick == 2)
+            {
+                RotateTransform rotateTransform = new RotateTransform(90);
+                Arrow.RenderTransform = rotateTransform;
 
-                if (i == 1)
-                {
-                    string sarrowdir = "up";
-                    TextBlock0.Text = "Direction : " + sarrowdir;
-                }
+                string sarrowdir = "bas";
+                TextBlock0.Text = "Direction : " + sarrowdir;
+            }
 
-                else if (i == 2)
-                {
-                    string sarrowdir = "down";
-                    TextBlock0.Text = "Direction : " + sarrowdir;
-                }
+            else if (pick == 3)
+            {
+                RotateTransform rotateTransform = new RotateTransform(180);
+                Arrow.RenderTransform = rotateTransform;
 
-                else if (i == 3)
-                {
-                    string sarrowdir = "left";
-                    TextBlock0.Text = "Direction : " + sarrowdir;
-                }
+                string sarrowdir = "gauche";
+                TextBlock0.Text = "Direction : " + sarrowdir;
+            }
 
-                else if (i == 4)
-                {
-                    string sarrowdir = "right";
-                    TextBlock0.Text = "Direction : " + sarrowdir;
-                }
+            else if (pick == 4)
+            {
+                RotateTransform rotateTransform = new RotateTransform(0);
+                Arrow.RenderTransform = rotateTransform;
 
-                /* key matching */
+                string sarrowdir = "droite";
+                TextBlock0.Text = "Direction : " + sarrowdir;
+            }
+
+            /* key matching */
                 
-                if (i == 1 && e.Key == Key.Z)
-                {
-                    TextBlock1.Text = "good up";
-                    points++;
-                    TextBlock2.Text = "Scores : " + points;
+            if (pick == 1 && e.Key == Key.Z)
+            {
+                TextBlock1.Background = Brushes.Green;
+                points++;
+                TextBlock2.Text = "Scores : " + points;
 
-                }
-
-                else if (i == 2 && e.Key == Key.S)
-                {
-                    TextBlock1.Text = "good down";
-                    points++;
-                    TextBlock2.Text = "Scores : " + points;
-                }
-
-                else if (i == 3 && e.Key == Key.Q)
-                {
-                    TextBlock1.Text = "good left";
-                    points++;
-                    TextBlock2.Text = "Scores : " + points;
-                }
-
-                else if (i == 4 && e.Key == Key.D)
-                {
-                    TextBlock1.Text = "good right";
-                    points++;
-                    TextBlock2.Text = "Scores : " + points;
-                }
-
-                else
-                {
-                    TextBlock1.Text = "bad";
-                    points = 0;
-                    TextBlock2.Text = "Scores : " + points;
-                }
             }
 
+            else if (pick == 2 && e.Key == Key.S)
+            {
+                TextBlock1.Background = Brushes.Green;
+                points++;
+                TextBlock2.Text = "Scores : " + points;
+            }
+
+            else if (pick == 3 && e.Key == Key.Q)
+            {
+                TextBlock1.Background = Brushes.Green;
+                points++;
+                TextBlock2.Text = "Scores : " + points;
+            }
+
+            else if (pick == 4 && e.Key == Key.D)
+            {
+                TextBlock1.Background = Brushes.Green;
+                points++;
+                TextBlock2.Text = "Scores : " + points;
+            }
+
+            else
+            {
+                TextBlock1.Background = Brushes.Red;
+                points--;
+                TextBlock2.Text = "Scores : " + points;
+                RotateTransform rotateTransform = new RotateTransform(0);
+                Arrow.RenderTransform = rotateTransform;
+                Arrow.Visibility = Visibility.Hidden;
+            }
+            
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            TextBlock0.Text = "Direction : ";
-            TextBlock1.Text = "";
-            TextBlock2.Text = "Scores : 0";
-            TextBlock3.Text = "";
-            TextBlock4.Text = "";
-        }
     }
 
     
