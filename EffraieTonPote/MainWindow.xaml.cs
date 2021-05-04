7f4aa64be210c1f18c09622d5cdcc454c2b7bc1e
Biozard01 <arthur.laforest33@gmail.com>
2021-05-04 12:12:37 +0200

    timer v1 / code clean up

--- EffraieTonPote/MainWindow.xaml.cs
+++ EffraieTonPote/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace EffraieTonPote
 {
@@ -24,44 +25,96 @@ namespace EffraieTonPote
         {
             InitializeComponent();
 
+            /* Init de la grid (affichage et dissimulation des éléments)*/
+
+            TextBlock0.Visibility = Visibility.Hidden;
+            TextBlock1.Background = Brushes.LightBlue;
+            TextBlock2.Visibility = Visibility.Hidden;
+            TextBlock3.Visibility = Visibility.Hidden;
+            
+            Start.Visibility = Visibility.Visible;
+            Stop.Visibility = Visibility.Hidden;
+            
+            Arrow.Visibility = Visibility.Hidden; 
+            RotateTransform rotateTransform = new(0);
+            Arrow.RenderTransform = rotateTransform;
+
+            /* variable global pour les points */
+
+            Application.Current.Properties["Points"] = 0;
+
+            /* timer */
+            
+            Application.Current.Properties["Time"] = "0";
+            string time = (string)Application.Current.Properties["Time"];
+
+            DispatcherTimer timer = new()
+            {
+                Interval = TimeSpan.FromSeconds(1)
+            };
+
+            timer.Tick += ElapsedTime;
+            timer.Start();
+
+            void ElapsedTime(object sender, EventArgs e)
+            {
+
+                if (time == "5")
+                {
+                    TextBlock1.Text = "ouah";
+                }
+
+            }
         }
 
         /* bouton play on-click */
         private void Play(object sender, RoutedEventArgs e)
         {
-            TextBlock0.Text = "Direction : ";
-            TextBlock1.Text = "";
+            TextBlock0.Visibility = Visibility.Visible;
             TextBlock1.Background = Brushes.LightBlue;
-            TextBlock2.Text = "Scores : 0";
-            TextBlock3.Text = "";
-            Arrow.Visibility = Visibility.Hidden;
-            RotateTransform rotateTransform = new RotateTransform(0);
-            Arrow.RenderTransform = rotateTransform;
+            TextBlock2.Visibility = Visibility.Visible;
+            TextBlock3.Visibility = Visibility.Visible;
+
+            Start.Visibility = Visibility.Hidden;
+            Stop.Visibility = Visibility.Visible;
         }
 
         /* bouton reset on-click */
         private void Reset(object sender, RoutedEventArgs e)
         {
-            TextBlock0.Text = "Direction : ";
+            TextBlock0.Visibility = Visibility.Hidden;
             TextBlock1.Background = Brushes.LightBlue;
-            TextBlock2.Text = "Scores : 0";
-            TextBlock3.Text = "";
+            TextBlock2.Visibility = Visibility.Hidden;
+            TextBlock3.Visibility = Visibility.Hidden;
+
+            Start.Visibility = Visibility.Visible;
+            Stop.Visibility = Visibility.Hidden;
+
             Arrow.Visibility = Visibility.Hidden;
-            RotateTransform rotateTransform = new RotateTransform(0);
+            RotateTransform rotateTransform = new(0);
             Arrow.RenderTransform = rotateTransform;
         }
         
         /* événements déclanché par la pression d'une touche */
         private void KeyHandler(object sender, KeyEventArgs e)
         {
-            List<int> arr = new List<int>();
-            int points = 0;
-            Random random = new Random();
+            /* affichage des éléments */
 
-            /* path dynamique à faire */
-            string picture = "C:\\Users\\arthu\\source\\repos\\ETP\\EffraieTonPote\\arrow.png";
+            TextBlock0.Visibility = Visibility.Visible;
+            TextBlock1.Background = Brushes.LightBlue;
+            TextBlock2.Visibility = Visibility.Visible;
+            TextBlock3.Visibility = Visibility.Visible;
 
-            Arrow.Source = new BitmapImage(new Uri(picture));
+            Start.Visibility = Visibility.Hidden;
+            Stop.Visibility = Visibility.Visible;
+
+            Arrow.Visibility = Visibility.Visible;
+
+            /* init des variables */
+
+            int points = (int)Application.Current.Properties["Points"];
+            List<int> arr = new();
+            Random random = new();
 
             for (int i = 0; i <= 4; i++)
             {
@@ -72,85 +125,73 @@ namespace EffraieTonPote
 
             TextBlock3.Text = "Gen Number " + pick;
 
-            /* arrow direction display */
+            /* path dynamique à faire (va chercher le bon user) */
+            string picture = "C:\\Users\\arthu\\source\\repos\\ETP\\EffraieTonPote\\arrow.png";
 
-            Arrow.Visibility = Visibility.Visible;
+            Arrow.Source = new BitmapImage(new Uri(picture));
+
+            /* determination de la direction de la flèche */           
 
             if (pick == 1)
             {
-                RotateTransform rotateTransform = new RotateTransform(-90);
-                Arrow.RenderTransform = rotateTransform;
-
                 string sarrowdir = "haut";
                 TextBlock0.Text = "Direction : " + sarrowdir;
+                
+                RotateTransform rotateTransform = new(-90);
+                Arrow.RenderTransform = rotateTransform;
             }
 
             else if (pick == 2)
             {
-                RotateTransform rotateTransform = new RotateTransform(90);
-                Arrow.RenderTransform = rotateTransform;
-
                 string sarrowdir = "bas";
                 TextBlock0.Text = "Direction : " + sarrowdir;
+                
+                RotateTransform rotateTransform = new(90);
+                Arrow.RenderTransform = rotateTransform;
             }
 
             else if (pick == 3)
             {
-                RotateTransform rotateTransform = new RotateTransform(180);
-                Arrow.RenderTransform = rotateTransform;
-
                 string sarrowdir = "gauche";
                 TextBlock0.Text = "Direction : " + sarrowdir;
+                
+                RotateTransform rotateTransform = new(180);
+                Arrow.RenderTransform = rotateTransform;
             }
 
             else if (pick == 4)
             {
-                RotateTransform rotateTransform = new RotateTransform(0);
-                Arrow.RenderTransform = rotateTransform;
-
                 string sarrowdir = "droite";
                 TextBlock0.Text = "Direction : " + sarrowdir;
-            }
-
-            /* key matching */
                 
-            if (pick == 1 && e.Key == Key.Z)
-            {
-                TextBlock1.Background = Brushes.Green;
-                points++;
-                TextBlock2.Text = "Scores : " + points;
-
+                RotateTransform rotateTransform = new(0);
+                Arrow.RenderTransform = rotateTransform;
             }
 
-            else if (pick == 2 && e.Key == Key.S)
+            /* test pour voir si les touche relaché correspondent aux bonne direction de flèches */
+                
+            if (pick == 1 && e.Key == Key.Z || pick == 2 && e.Key == Key.S || pick == 3 && e.Key == Key.Q || pick == 4 && e.Key == Key.D)
             {
-                TextBlock1.Background = Brushes.Green;
                 points++;
                 TextBlock2.Text = "Scores : " + points;
-            }
-
-            else if (pick == 3 && e.Key == Key.Q)
-            {
                 TextBlock1.Background = Brushes.Green;
-                points++;
-                TextBlock2.Text = "Scores : " + points;
-            }
-
-            else if (pick == 4 && e.Key == Key.D)
-            {
-                TextBlock1.Background = Brushes.Green;
-                points++;
-                TextBlock2.Text = "Scores : " + points;
-            }
-
+            } 
+            
             else
             {
-                TextBlock1.Background = Brushes.Red;
                 points--;
                 TextBlock2.Text = "Scores : " + points;
-                RotateTransform rotateTransform = new RotateTransform(0);
-                Arrow.RenderTransform = rotateTransform;
+
+                TextBlock1.Background = Brushes.Red;
+                
+                /* affichage des éléments */
+
                 Arrow.Visibility = Visibility.Hidden;
+                RotateTransform rotateTransform = new(0);
+                Arrow.RenderTransform = rotateTransform;
+
+                Start.Visibility = Visibility.Hidden;
+                Stop.Visibility = Visibility.Visible;
             }
             
         }
