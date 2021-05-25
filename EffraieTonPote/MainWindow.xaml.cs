08eec7ca9806ce90b4665205b82f5909b9934f23
Biozard01 <arthur.laforest33@gmail.com>
2021-05-25 14:58:32 +0200

    final fix

--- EffraieTonPote/MainWindow.xaml.cs
+++ EffraieTonPote/MainWindow.xaml.cs
@@ -24,66 +24,98 @@ namespace EffraieTonPote
 	/// </summary>
 	public partial class MainWindow : Window
 	{
-		TextBoxOutputter outputter;
 		public MainWindow()
 		{
 			InitializeComponent();
-			outputter = new TextBoxOutputter(TestBox);
-			Console.SetOut(outputter);
-			Console.WriteLine("Started");	
+		
 		}
 
 		/* Bouton play on-click */
 		private void Start(object sender, RoutedEventArgs e)
 		{
-			/* Path dynamique */
-            string picture = "Assets\\arrow.png";
-            string path = System.IO.Path.Combine(Environment.CurrentDirectory.Replace("\\bin\\Debug\\net5.0-windows", ""), picture);
-
-            Arrow.Source = new BitmapImage(new Uri(path));
-
 			/* Variable global pour les points */
 			Application.Current.Properties["Points"] = 0;
 
 			/* Affichage des éléments */
 			Play.Visibility = Visibility.Hidden;
 			Arrow.Visibility = Visibility.Visible;
+			TextBlock0.Visibility = Visibility.Visible;
+
+			/* Path dynamique */
+			string picture = "Assets\\arrow.png";
+            string path = System.IO.Path.Combine(Environment.CurrentDirectory.Replace("\\bin\\Debug\\net5.0-windows", ""), picture);
+
+            Arrow.Source = new BitmapImage(new Uri(path));
 			
-			Console.WriteLine("start1");
-
 			/* Determination de la direction de la flèche*/
-			ArrDirect arrDirect = new ArrDirect();
-
-			Console.WriteLine("start2");
+			ArrDir();
 
 			/* Événements déclanché par la pression d'une touche */
-			KeyHandling keyHandling = new KeyHandling();
-			KeyUp += keyHandling.KeyHandler;
-
-			Console.WriteLine("start3");
+			KeyUp += KeyHandler;
 		}
 
-		public class TextBoxOutputter : TextWriter
+		/* Événements déclanché par la pression d'une touche */
+		public void KeyHandler(object sender, KeyEventArgs e)
 		{
-			TextBox textBox = null;
+			/* Test pour voir si les touche relaché correspondent aux bonne direction de flèches */
+			int pick = (int)Application.Current.Properties["Pick"];
+			int points = (int)Application.Current.Properties["Points"];
 
-			public TextBoxOutputter(TextBox output)
+			/*Determination de la direction de la flèche*/
+			ArrDir();
+
+			if (pick == 1 && e.Key == Key.Z || pick == 2 && e.Key == Key.S || pick == 3 && e.Key == Key.Q ||
+			   pick == 4 && e.Key == Key.D) 
 			{
-				textBox = output;
+				points++;
+				Application.Current.Properties["Points"] = points;
+				TextBlock0.Text = "Scores : " + points;
+				TextBlock1.Background = Brushes.LightGreen;
 			}
 
-			public override void Write(char value)
+			else
 			{
-				base.Write(value);
-				textBox.Dispatcher.BeginInvoke(new Action(() =>
-				{
-					textBox.AppendText(value.ToString());
-				}));
+				Application.Current.Properties["Points"] = 0;
+							
+				Environment.Exit(1);
 			}
+		}
 
-			public override Encoding Encoding
+		/* Determination de la direction de la flèche */
+		public void ArrDir()
+		{
+			/* Détermin pick */
+			Random random = new();
+			Application.Current.Properties["Pick"] = random.Next(1, 5);
+			int pick = (int)Application.Current.Properties["Pick"];
+
+			switch (pick)
 			{
-				get { return System.Text.Encoding.UTF8; }
+				case 1:
+					RotateTransform rotateTransform = new(-90);
+					Arrow.RenderTransform = rotateTransform;
+					Arrow.HorizontalAlignment = HorizontalAlignment.Center;
+					Arrow.VerticalAlignment = VerticalAlignment.Center;
+					break;
+
+				case 2:
+					rotateTransform = new(90);
+					Arrow.RenderTransform = rotateTransform;
+					Arrow.HorizontalAlignment = HorizontalAlignment.Center;
+					Arrow.VerticalAlignment = VerticalAlignment.Center;
+					break;
+
+				case 3:
+					rotateTransform = new(180);
+					Arrow.RenderTransform = rotateTransform;
+					Arrow.HorizontalAlignment = HorizontalAlignment.Center;
+					Arrow.VerticalAlignment = VerticalAlignment.Center;
+					break;
+
+				default:
+					rotateTransform = new(0);
+					Arrow.RenderTransform = rotateTransform;
+					break;
 			}
 		}
 	}
